d362c95c5d63c3f69e1cabe335afc483ff7a1a89
Vipershot <imvipershot@gmail.com>
2022-02-08 15:29:18 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect,useState} from 'react';
+
 
 function App() {
+
+
+  const [personajes, setPersonajes] = useState(null);
+  const [nombre, setNombre] = useState('maikol');
+  const [valor, setValor] = useState('');
+
+  const fetchApi = async() => {
+  const peticion = await fetch('https://rickandmortyapi.com/api/character')
+  const resultado = await peticion.json()
+  const characters = resultado.results
+  setPersonajes(characters)
+  }
+  
+  useEffect(() => {
+    console.log(personajes)
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {nombre}
+      <input placeholder='Escribe Tu Nombre' onChange={(e)=>{setValor(e.target.value)}}></input>
+      <button onClick={()=>{
+        setNombre(valor) 
+        setValor('')}
+        } >Cambiar nombre</button>
+      <h1>Api</h1>
+      {personajes === null ? null : personajes.map((personaje)=> (<h1 key={personaje.name}> {personaje.name} </h1>))}
+
+
+      {personajes === null ?  <button onClick={()=>{fetchApi()}}>Ver Personajes</button> : null}
+    
     </div>
   );
 }
